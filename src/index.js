5956b29bb27878d0f1c410e801888d21ea484925
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2022-07-20 01:58:25 -0300

    remover conta

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ Query Params - Paginação / Filtro
 Body Params - Os objetos inserção/alteração (JSON)
 */
 
+const { request, response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid")
 
@@ -142,6 +143,22 @@ app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
     const { customer } = request;
 
     return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
 });
 
 app.listen(3333);
